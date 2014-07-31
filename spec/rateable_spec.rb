13c277ad84c4ad014d6b462d15da2364912eab6a
Przemysław Wróblewski <przemyslaw.wroblewski@nokaut.pl>
2014-07-31 12:48:30 +0200

    check if rater is being saved

--- spec/rateable_spec.rb
+++ spec/rateable_spec.rb
@@ -12,8 +12,10 @@ describe Rateable do
   it 'must handle rating' do
     @model.rate!(5)
     @model.current_rating.must_equal 5.0
-    @model.rate!(1)
+    rater_ip = '192.168.1.1'
+    @model.rate!(1, rater_ip)
     @model.current_rating.must_equal 3.0
+    @model.rating.rating_marks.select { |rm| rm.rater == rater_ip }.size.must_equal 1
   end
 
   it 'returns nil when no rates were casted' do
